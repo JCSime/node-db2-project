efb658ee687582be0b5df84493e21b022cf61361
John S <johnc.sime@gmail.com>
2022-02-15 14:38:39 -0500

    created seed file and created up function in migrations folder

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,29 @@
 // STRETCH
+const cars = [
+    {
+        vin: '1234567890123',
+        make: 'Nissan',
+        model: '350z',
+        mileage: 60000,
+        title: 'clean',
+        transmission: 'manual',
+    },
+    {
+        vin: '1234567890123',
+        make: 'Toyota',
+        model: 'AE86',
+        mileage: 3000,
+        title: 'clean',
+    },
+    {
+        vin: '1234567890123',
+        make: 'Ford',
+        model: 'Crown Vic',
+        mileage: 265000,
+    }
+]
+
+exports.seed = async function(knex) {
+    await knex('cars').truncate()
+    await knex('cars').insert(cars)
+}
