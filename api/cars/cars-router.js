9cc161575b3345c5ba689423507c5f075058a393
John S <johnc.sime@gmail.com>
2022-02-15 16:41:51 -0500

    checkCarPayload middleware has been setup

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -2,7 +2,12 @@
 const express = require('express')
 const Car = require('./cars-model')
 const router = express.Router();
-const { checkCarId } = require('./cars-middleware')
+const { 
+  checkCarId,
+  checkCarPayload,
+  checkVinNumberValid,
+  checkVinNumberUnique, 
+} = require('./cars-middleware')
 
 router.get('/', async (req, res, next) =>{
   try {
@@ -16,7 +21,7 @@ router.get('/', async (req, res, next) =>{
 router.get('/:id', checkCarId, async (req, res, next) =>{
   res.json(req.car)
 });
-router.post('/', async (req, res, next) =>{
+router.post('/', checkCarPayload, checkVinNumberValid, checkVinNumberUnique, async (req, res, next) =>{
   res.json('posting new car')
 })
 
